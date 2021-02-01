09636f4341ba9965b87e1c1b75fda5e3a57900a0
sabu shakya <sabu.shakya@f1soft.com>
2021-02-01 16:33:07 +0545

    Using context from springapplication run

--- bean-life-cycle/src/main/java/com/sabu/learning/beanlifecycle/BeanLifeCycleApplication.java
+++ bean-life-cycle/src/main/java/com/sabu/learning/beanlifecycle/BeanLifeCycleApplication.java
@@ -4,18 +4,22 @@ import com.sabu.learning.beanlifecycle.beans.Config;
 import com.sabu.learning.beanlifecycle.beans.HookExampleUsingInterfaces;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class BeanLifeCycleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeanLifeCycleApplication.class, args);
+        // SpringApplication run will also return an application context.
+        ApplicationContext context = SpringApplication.run(BeanLifeCycleApplication.class, args);
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         HookExampleUsingInterfaces hookExampleUsingInterfaces = context.getBean(HookExampleUsingInterfaces.class);
         hookExampleUsingInterfaces.hookExampleUsingInterfaces();
-        context.close();
+//        context.close();
+        ((ConfigurableApplicationContext)context).close();
     }
 
 }
