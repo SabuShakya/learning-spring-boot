fdc03a901f8564e9bbef771f0cb6b367dd036168
sabu shakya <sabu.shakya@f1soft.com>
2021-02-01 14:54:02 +0545

    Bean lifecycle

---
+++ bean-life-cycle/src/main/java/com/sabu/learning/beanlifecycle/BeanLifeCycleApplication.java
@@ -0,0 +1,21 @@
+package com.sabu.learning.beanlifecycle;
+
+import com.sabu.learning.beanlifecycle.beans.Config;
+import com.sabu.learning.beanlifecycle.beans.HookExampleUsingInterfaces;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class BeanLifeCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanLifeCycleApplication.class, args);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        HookExampleUsingInterfaces hookExampleUsingInterfaces = context.getBean(HookExampleUsingInterfaces.class);
+        hookExampleUsingInterfaces.hookExampleUsingInterfaces();
+        context.close();
+    }
+
+}
